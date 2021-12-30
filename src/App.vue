285c3e63f632dbdf89a8b7c1b33ada1eebf2274a
frankfang <frankfang1990@gmail.com>
2021-12-30 15:04:44 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+const b = ref(false)
+</script>
+
+<template>
+  <button @click="b = !b">b is {{ b }}</button>
+  <HelloWorld :loading="b" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
